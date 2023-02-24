1d8e15e96b50e17edd139fd273e23c5a81e502e2
angelherce <angel.herce@imatia.com>
2023-02-24 11:22:11 +0100

    Sección 03: V025 - Constructor de una clase - Se ha actualizado el fichero TypeScript con el ejercicio 10 de la sección.

--- projects/owner/ts-intro/src/ejercicios/10-basic-class.ts
+++ projects/owner/ts-intro/src/ejercicios/10-basic-class.ts
@@ -2,11 +2,31 @@
     ===== Código de TypeScript =====
 */
 
-class SHeroe {
+class S1Heroe {
     private alter: string;
     public year: number;
     public name: string;
+
+    public constructor( alter: string, year: number, name: string ){
+        console.log( 'Hey !!' );
+        this.alter = alter;
+        this.year = year;
+        this.name = name;
+    }
 }
 
-const ironman: SHeroe = new SHeroe();
+class S2Heroe {
+    public constructor(
+        private alter: string,
+        public year: number,
+        public name: string ){
+
+        console.log( 'Hey !!' );
+    }
+}
+
+const spiderman: S1Heroe = new S1Heroe('SpiderMan', 30, 'Peter Parker' );
+const ironman: S2Heroe = new S2Heroe('IronMan', 45, 'Tony Stark' );
+
+console.log( spiderman );
 console.log( ironman );
