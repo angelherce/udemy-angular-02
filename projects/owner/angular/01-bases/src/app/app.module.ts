8613a55d7a44965556bc96eb31606144f9802eb2
angelherce <angel.herce@imatia.com>
2023-02-27 09:18:00 +0100

    Sección 04: V043 - Componente de Heroe y separación de directorios - Se ha creado el componente Heroe manualmente

--- projects/owner/angular/01-bases/src/app/app.module.ts
+++ projects/owner/angular/01-bases/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent
+    CounterComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule
