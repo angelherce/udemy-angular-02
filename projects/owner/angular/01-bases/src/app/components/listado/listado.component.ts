bd209bbb3d4a7bffe2924b1d4b3e04412dd46ba9
angelherce <angel.herce@imatia.com>
2023-03-02 09:35:27 +0100

    Sección 04: V047 - Directiva *ngFor - Se ha actualizado el componente listado con las tareas y experimentos realizados durante el video

--- projects/owner/angular/01-bases/src/app/components/listado/listado.component.ts
+++ projects/owner/angular/01-bases/src/app/components/listado/listado.component.ts
@@ -7,12 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListadoComponent implements OnInit {
 
+  public heroDeleted: string = '';
+  public heroesBackup: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor'];
+  public heroes: string[] = [];
+
   public constructor() {
     console.log( 'Constructor !' );
+    this.fill();
   }
 
   public ngOnInit(): void {
     console.log( 'ngOnInit !' );
   }
 
+  public deleteFirst(): void{
+    this.heroDeleted = this.heroes.shift() || '';
+  }
+
+  public deleteAll(): void{
+    let heroesDeleted = '';
+    this.heroes.forEach( (target, index) => {
+      if( index == 0 ){
+        heroesDeleted = `${target}`
+      }
+      else{
+        heroesDeleted = `${heroesDeleted}, ${target}`
+      }
+    });
+    this.heroes = [];
+    this.heroDeleted = heroesDeleted;
+  }
+
+  public deleteLast(): void{
+    this.heroDeleted = this.heroes.pop() || '';
+  }
+
+  public invert(): void{
+    let result: string[] = [];
+    this.heroes.forEach( target => result.unshift( target ));
+    this.heroes = result;
+    this.heroDeleted = '';
+  }
+
+  public fill(): void{
+    this.deleteAll();
+    this.heroDeleted = '';
+    this.heroesBackup.forEach( target => this.heroes.push( target ));
+  }
 }
