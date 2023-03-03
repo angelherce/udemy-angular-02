33f83b9d4df5d049b43deef56fec836ea0455e82
angelherce <angel.herce@imatia.com>
2023-03-03 09:11:37 +0100

    Sección 05: V060 - ngModel - Se ha configurado el método "changeName" del componente Home del módulo Dragon Ball Z para capturar el nuevo valor de los campo name del forumlario mediante el evento "input".

--- projects/owner/angular/01-bases/src/app/modules/dbz/components/home/home.component.ts
+++ projects/owner/angular/01-bases/src/app/modules/dbz/components/home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Character{
+  name: string,
+  power: number
+}
+
 @Component({
   selector: 'dbz-home',
   templateUrl: './home.component.html',
@@ -9,6 +14,11 @@ export class HomeComponent implements OnInit {
 
   public title: string = 'Dragon Ball Z';
 
+  public newCharacter: Character = {
+    name: 'Trunks',
+    power: 14000
+  }
+
   public constructor() { }
 
   public ngOnInit(): void {
@@ -16,5 +26,10 @@ export class HomeComponent implements OnInit {
 
   public save(): void{
     console.log( "Hi World !" );
+  }
+
+  public changeName( event: any ): void{
+    this.newCharacter.name = event.target.value;
+    console.log( event )
   }
 }
