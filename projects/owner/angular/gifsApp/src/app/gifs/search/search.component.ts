a5d74102ee4e0ab61b80999b578b7e064a68d209
angelherce <angel.herce@imatia.com>
2023-03-06 11:52:59 +0100

    Sección 07: V083 - @ViewChild - Obtener referencias a objetos del HTML - Se ha establecido el método search en el componente Search para poder recuperar la información que se introduzca en el input del componente Search.

--- projects/owner/angular/gifsApp/src/app/gifs/search/search.component.ts
+++ projects/owner/angular/gifsApp/src/app/gifs/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild( 'inputSearch' )
+  public inputSearch!: ElementRef<HTMLInputElement>;
 
-  ngOnInit(): void {
+  public constructor() { }
+
+  public ngOnInit(): void {
   }
 
+  public search(): void{
+    console.log( this.inputSearch.nativeElement.value );
+    this.inputSearch.nativeElement.value = '';
+  }
 }
