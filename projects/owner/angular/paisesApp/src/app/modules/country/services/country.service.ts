a694b4b0043f419e3759f6c8b9c64c992f85ce46
angelherce <angel.herce@imatia.com>
2023-03-07 10:17:59 +0100

    Sección 08: V110 - Tipado de la petición RestCountries - Se ha añadido la interfaz "Country" y todas las necesarias con la ayuda de Quicktype.

--- projects/owner/angular/paisesApp/src/app/modules/country/services/country.service.ts
+++ projects/owner/angular/paisesApp/src/app/modules/country/services/country.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError, Observable, of } from 'rxjs';
+import { Country } from '../interfaces/country.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +14,8 @@ export class CountryService {
 
   public constructor( private http: HttpClient ) {}
 
-  public searchByCountry( value: string ): Observable<any>{
+  public searchByCountry( value: string ): Observable<Country[]>{
     const url = `${this.apiUrl}/name/${value}`;
-    return this.http.get( url ); //.pipe( catchError( error => of([])));
+    return this.http.get<Country[]>( url ); //.pipe( catchError( error => of([])));
   }
 }
